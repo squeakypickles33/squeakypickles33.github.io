fbef5d4216d5edc75ba6ceabcb2200cdf04fbb4d
derekwilliams <dwilliams33dw@gmail.com>
2024-05-25 18:46:22 -0700

    fetching

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -7,7 +7,7 @@ import WorkExperience from "./workExperience/WorkExperience";
 import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
-import Blogs from "./blogs/Blogs";
+// import Blogs from "./blogs/Blogs";
 import Footer from "../components/footer/Footer";
 import Talks from "./talks/Talks";
 import Podcast from "./podcast/Podcast";
@@ -59,7 +59,7 @@ const Main = () => {
             <Achievement />
             <Education />
             <StackProgress />
-            <Blogs />
+            {/* <Blogs /> */}
             <Talks />
             <Twitter />
             <Podcast />
